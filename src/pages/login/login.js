7f3dfef8a675ed639d896c7526b13cf85d3eae04
dustin1709 <56805815+dustin1709@users.noreply.github.com>
2022-04-17 22:34:15 -0400

    Add comments: Login.js

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,30 +4,41 @@ import { useNavigate, Navigate, Link } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
+    
+    // setting labels values, and their setters for useState
     const [ username,setUsername ] = useState('');
     const [ password,setPassword ] = useState('');
     const [ errSMS, setErrSMS ] = useState('');
 
     const API_URL = 'http://192.168.192.31:3000/users';
 
+    // if the user is saved, navigate to the homepage
     useEffect(() => {
         if (localStorage.getItem("user")) {
             navigate('/dashboard');
         }
     }, [])
 
+    // Login function
     const login = async (e) => {
         e.preventDefault();
         let result = await fetch(API_URL);
         if (!result.ok) throw Error('Did not receive expected data');
+        
+        // set the list of users if we do not get the error message
         let response = await result.json();
         let users = response.item_typE;
+        
+        // Compare from the list of users
         users.map((user) => {
             if(username == user.username && password == user.password) {
                 navigate('/dashboard');
                 localStorage.setItem("user", user.username);
                 localStorage.setItem("user-id", user.universityID);
             } else {
+                // if login is invalid, set error message on screen
+                // errSMS will appear under the h3 tag "RIT CageLab Log in"
+                // if condition wasn't meet
                 setErrSMS('Invalid credentials. Please try again.');
             }
         });
